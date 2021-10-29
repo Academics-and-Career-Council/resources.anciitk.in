7fc4e70704839715ee697d50f3b8214d18409e1f
vakshit <akshitv18@gmail.com>
2021-10-29 21:26:56 +0530

    fixed redirect

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,27 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RelayEnvironmentProvider } from "react-relay";
+import GA4React from "ga-4-react";
 
 import Environment from "./graphql/environment";
 import { RecoilRoot } from "recoil";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <RelayEnvironmentProvider environment={Environment}>
-        <App />
-      </RelayEnvironmentProvider>
-    </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const ga4react = new GA4React("G-WK2PMRSK9H");
+
+(async () => {
+  await ga4react.initialize();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <RecoilRoot>
+        <RelayEnvironmentProvider environment={Environment}>
+          <App />
+        </RelayEnvironmentProvider>
+      </RecoilRoot>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+})();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
